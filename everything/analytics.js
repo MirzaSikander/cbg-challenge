f0ceeb89bbd7a67c1c24640637f35b872a33434d
Mirza Sikander <mirzasikander@MirzaAir.(none)>
2013-09-08 00:53:46 -0700

    able to detect where the shot was started from

--- everything/analytics.js
+++ everything/analytics.js
@@ -26,7 +26,15 @@ function findShots(){
 		if( boundbox1.z <= ballz && ballz <= boundbox1.z + bbDim.height ){
 			if(boundbox1.y <= bally && bally <=boundbox1.y + bbDim.width){
 				if(boundbox1.x <= ballx && ballx <= boundbox1.x + bbDim.length){
-					shot_frames.push(i)
+					//going back
+					//assumption: the ball must have started at the floor sometime
+					//in the past
+					var j = i;
+					for(var another_ballz = ballz; another_ballz > 5; j-- ){
+						 another_ballz = data[j].ball[2] 
+					}
+					if(shot_frames.indexOf(j) < 0)
+						shot_frames.push(j)
 				}
 			}
 		}
@@ -34,7 +42,15 @@ function findShots(){
 		if( boundbox2.z <= ballz && ballz <= boundbox2.z + bbDim.height ){
 			if(boundbox2.y <= bally && bally <=boundbox2.y + bbDim.width){
 				if(boundbox2.x <= ballx && ballx <= boundbox2.x + bbDim.length){
-					shot_frames.push(i)
+					//going back
+					//assumption: the ball must have started at the floor sometime
+					//in the past
+					var j = i;
+					for(var another_ballz = ballz; another_ballz > 5; j-- ){
+						 another_ballz = data[j].ball[2] 
+					}
+					if(shot_frames.indexOf(j) < 0)
+						shot_frames.push(j)
 				}
 			}
 		}
